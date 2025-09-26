a185758b58f994cc9e127e482fe3bfaa7a7b9ad5
Tommy <rossithomashernan@gmail.com>
2025-09-26 15:41:21 -0300

    fix config para Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,11 +10,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true, // Limpia la carpeta antes de build
-    chunkSizeWarningLimit: 1600, // Opcional: ajusta límite de warnings
-  },
-  preview: {
-    port: 3000,
-    host: true
+    sourcemap: false
   }
 })
