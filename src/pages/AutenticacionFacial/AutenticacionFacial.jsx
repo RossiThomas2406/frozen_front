332c34e8e1a83c4a750cdcfbe4f6e5ccbb18c361
RossiThomas2406 <rossithomashernan@gmail.com>
2025-09-28 20:59:20 -0300

    cambios autenticacion umbral

--- src/pages/AutenticacionFacial/AutenticacionFacial.jsx
+++ src/pages/AutenticacionFacial/AutenticacionFacial.jsx
@@ -72,7 +72,7 @@ function AutenticacionFacial() {
                 canvas.height = videoFeedEl.videoHeight;
                 canvas.width = videoFeedEl.videoWidth;
                 
-                const faceMatcher = new faceapi.FaceMatcher(vectoresUsuario, 0.2);
+                const faceMatcher = new faceapi.FaceMatcher(vectoresUsuario, 0.4);
                 
                 // Limpiar intervalo previo si existe
                 if (intervalRef.current) {
