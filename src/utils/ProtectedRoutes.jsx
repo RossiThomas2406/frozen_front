9c32eced1ea1f9398e686f4e78476f55ca1608bf
RossiThomas2406 <rossithomashernan@gmail.com>
2025-09-27 22:10:24 -0300

    fix protected routes

--- src/utils/ProtectedRoutes.jsx
+++ src/utils/ProtectedRoutes.jsx
@@ -4,7 +4,7 @@ import { Outlet, Navigate } from "react-router-dom";
 const ProtectedRoutes = () =>{
     const user = localStorage.getItem('usuario');
 
-    return user ?  <Outlet></Outlet> : <Navigate to={"/login"}></Navigate>
+    return user ?  <Outlet></Outlet> : <Navigate to={"/"}></Navigate>
 }
 
 export default ProtectedRoutes
